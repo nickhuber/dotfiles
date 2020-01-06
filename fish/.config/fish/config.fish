476d6f7e35c73c36ed83e58b041389bc86ce0677
Nick Huber <nick@nickhuber.ca>
2020-01-06 08:43:25 -0800

    Some fish changes, newer alacritty and a wishlist

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,9 +3,12 @@ set fish_greeting "¯\_(ツ)_/¯"
 set -x DEBFULLNAME "Nick Huber"
 set -x DEBEMAIL "nick.huber@multapplied.net"
 
-set -U fish_user_paths /home/nick/.local/bin /home/nick/bin $fish_user_paths
+set -U fish_user_paths /home/nick/.local/bin /home/nick/bin
 
 export EDITOR=nvim
 export NETRC=/home/nick/.netrc
 
 umask 0002
+
+# Support wide unicode characters
+set -g fish_emoji_width 2
